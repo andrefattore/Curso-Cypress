2890448273dfca9dbde5cf4c0fcd0e15daac2d55
Andre Garcia Fattore <andref@maceng.mac.engenharia.com.br>
2023-08-29 15:47:16 -0300

    Aula de sincronismo

--- cypress/e2e/elements.spec.cy.js
+++ cypress/e2e/elements.spec.cy.js
@@ -42,65 +42,38 @@ describe("Trabalhando com Elementos Básicos", () => {
   //fim describe
 });
 
-
 describe("Teste de manipulação de inputs de texto", () => {
   before(() => {
     cy.visit("https://wcaquino.me/cypress/componentes.html");
   });
 
   it("Campo de texto", () => {
-    cy.get("#formNome")
-      .type("Teste")
-      .should("have.value", "Teste");
+    cy.get("#formNome").type("Teste").should("have.value", "Teste");
 
-    //Quando o id tiver ':', colocar duas barras para que nenhum erro aconteça  
+    //Quando o id tiver ':', colocar duas barras para que nenhum erro aconteça
     cy.get("#elementosForm\\:sugestoes")
       .type("Teste")
       .should("have.value", "Teste");
 
-  //tabelas
-  cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
-    .type('???');
+    //tabelas
+    cy.get(
+      "#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input"
+    ).type("???");
 
-  cy.get('[data-cy="dataSobrenome"]')
-    .type('Teste12345{backspace}{backspace}')
-    .should('have.value',"Teste123");
-    
+    cy.get('[data-cy="dataSobrenome"]')
+      .type("Teste12345{backspace}{backspace}")
+      .should("have.value", "Teste123");
+
     cy.get("#elementosForm\\:sugestoes")
       .clear()
-      .type('Erro{selectall}acerto',{delay:100})
-      .should('have.value','acerto');
+      .type("Erro{selectall}acerto", { delay: 100 })
+      .should("have.value", "acerto");
   });
 
   //fim describe
 });
 
-
-
-
-describe('Manipulação de Radio Buttons',()=>{
-
-//Antes de Tudo
-before(() => {
-  //visita a página
-  cy.visit("https://wcaquino.me/cypress/componentes.html");
-});
-
-//Antes de executar cada teste
-beforeEach(() => {
-  //recarrega a página
-  cy.reload();
-});
-
-
-  it('Validar Radio Buttons',()=>{
-    cy.get('#formSexoMasc').check()
-  });
-  
-});
-
-describe('Manipulando Combo box', ()=>{
-
+describe("Manipulação de Radio Buttons", () => {
   //Antes de Tudo
   before(() => {
     //visita a página
@@ -113,19 +86,33 @@ describe('Manipulando Combo box', ()=>{
     cy.reload();
   });
 
-  it('Combo',()=>{
-  
-    
-    cy.get('[data-test="dataEscolaridade"]')
-        .select('2o grau completo')
-        .should('have.value', '2graucomp'); //verificar o 'value' por meio das ferramentas de desenvolvedor
-    
-    cy.get('[data-test="dataEscolaridade"]')
-        .select('1graucomp')
-        .should('have.value', '1graucomp'); 
- 
+  it("Validar Radio Buttons", () => {
+    cy.get("#formSexoMasc").check();
   });
-  
-  
+});
+
+describe("Manipulando Combo box", () => {
+  //Antes de Tudo
+  before(() => {
+    //visita a página
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
+
+  //Antes de executar cada teste
+  beforeEach(() => {
+    //recarrega a página
+    cy.reload();
+  });
+
+  it("Combo", () => {
+    cy.get('[data-test="dataEscolaridade"]')
+      .select("2o grau completo")
+      .should("have.value", "2graucomp"); //verificar o 'value' por meio das ferramentas de desenvolvedor
+
+    cy.get('[data-test="dataEscolaridade"]')
+      .select("1graucomp")
+      .should("have.value", "1graucomp");
+  });
+
   //fim describe
-  });
+});
---
+++ cypress/e2e/sync.spec.cy.js
@@ -0,0 +1,54 @@
+describe("Esperas...", () => {
+    //Antes de Tudo
+    beforeEach(() => {
+      //visita a página
+      cy.visit("https://wcaquino.me/cypress/componentes.html");
+    });
+  
+    //Antes de executar cada teste
+    beforeEach(() => {
+      //recarrega a página
+      cy.reload();
+    });
+
+    it('Deve aguardar o elemento estar disponível',()=>{
+        cy.get('#novoCampo').should('not.exist');
+        cy.get('#buttonDelay').click();
+        cy.get('#novoCampo').should('not.exist');
+        cy.get('#novoCampo').should('exist');
+        cy.get('#novoCampo').type('funciona');
+    });
+
+    it('Uso do find', ()=>{
+        cy.get('#buttonList').click();
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1');
+
+       /* cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 2');*/
+
+            cy.get('#lista li span')
+            .should('contain', 'Item 2');
+    });
+
+    it ('Uso do Timeout', ()=>{
+        cy.get('#buttonDelay').click();
+        cy.get('#novoCampo', {timeout:5000}).should('exist');
+    });
+
+    it('Uso do wait', ()=>{
+        cy.get('#buttonList').click();
+        cy.wait(5000);
+        cy.get('#lista li span')
+        .should('contain', 'Item 2');
+    });
+
+    it('Click retry', ()=>{
+        cy.get('#buttonCount')
+            .click()
+            .should('have.value','1');
+    });
+
+});
